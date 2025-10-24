108ef080f33ef54d4ce643a9ad21c0ac5bfcf903
yuichiro-y <ympe0624@gmail.com>
2025-10-24 21:21:03 +0900

    課題2実装

---
+++ src/Detail.js
@@ -0,0 +1,31 @@
+import { useParams } from "react-router-dom";
+import {posts} from "./data/posts";
+
+export const PostDetail = () => {
+  const { id } = useParams();
+  const post = posts.find(p => p.id === Number(id));
+
+  return (
+    <article className="max-w-2xl mx-auto my-10 px-2">
+      <div key={post.id}>
+        <img src={post.thumbnailUrl} className="mb-5"/>
+
+        <div className="px-5">
+          <div className="flex justify-between">
+            <div className="text-gray-500 text-sm">{new Date(post.createdAt).toLocaleDateString('ja-JP')}
+            </div>
+            {post.categories && (<ul className="flex font-semibold text-sm">{post.categories.map((c)=>
+              <li className="border border-blue-500 text-blue-500 rounded-md mr-1.5 px-1.5 py-0.5">{c}</li>)}</ul>)}
+            </div>
+
+          <div className="text-left">
+            <h1 className="text-2xl py-3">{post.title}</h1>
+            <div className=""dangerouslySetInnerHTML={{ __html: post.content }} />
+          </div>
+          
+        </div>
+
+      </div>
+    </article>
+  );
+};
--- src/routeLink.js
+++ src/routeLink.js
@@ -1,8 +1,7 @@
 import { BrowserRouter ,Routes, Route} from "react-router-dom"
 import App from "./App"
-import { Header } from "./Header"
 import { TopPage } from "./TopPage"
-
+import { PostDetail } from "./Detail"
 
 export const RouteLink = () => {
   return (
@@ -10,6 +9,7 @@ export const RouteLink = () => {
       <Routes>
         <Route element={<App />}>
           <Route index element={<TopPage />}/>
+            <Route path="posts/:id" element={<PostDetail />} />
         </Route>
       </Routes>
     </BrowserRouter>
