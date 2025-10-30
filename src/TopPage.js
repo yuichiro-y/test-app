ddbe9e7399066f6f113eb6ffd8ceb251938e99ee
yuichiro-y <ympe0624@gmail.com>
2025-10-30 21:22:09 +0900

    課題3実装

--- src/TopPage.js
+++ src/TopPage.js
@@ -4,17 +4,48 @@ import { Link } from "react-router-dom";
 
 export const TopPage = () => {
 
-  const [posts, setPosts] = useState([])
-  
+  const [posts , setPosts] = useState([]);
+  const [loading , setLoading] = useState(true);
+  const [error , setError] = useState(false);
   useEffect(() => {
     const fetcher = async() => {
-      const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
-      const data = await res.json()
-      setPosts(data.posts)
+      try {
+        setLoading(true);
+        const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
+
+        if (!res.ok) {
+          throw new Error(`HTTPエラー: ${res.status}`);
+        }
+        const data = await res.json()
+        setPosts(data.posts)
+
+      } catch (err) {
+        console.log(err);
+        setError(true);
+
+      } finally {
+        setLoading(false);
+      }
     }
 
     fetcher()
   },[])
+
+  if(loading === true){
+  return(
+      <div>
+        <p className="p-5">読み込み中...</p>
+      </div>
+  );
+ }
+
+  if (error) {
+    return (
+      <div>
+        <p className="p-5">データが取得できません。</p>
+      </div>
+    );
+  }
 
   return (
     <>
@@ -27,7 +58,7 @@ export const TopPage = () => {
               <div className="flex justify-between">
                 <div className="text-gray-500 text-sm">{new Date(post.createdAt).toLocaleDateString('ja-JP')}</div>
                 {post.categories && (<ul className="flex font-semibold text-sm">
-                  {post.categories.map((c)=><li className="border border-blue-500 text-blue-500 rounded-md mr-1.5 px-1.5 py-0.5">{c}</li>)}</ul>)}
+                  {post.categories.map((c)=><li key={c} className="border border-blue-500 text-blue-500 rounded-md mr-1.5 px-1.5 py-0.5">{c}</li>)}</ul>)}
               </div>
 
               <div className="text-left">
